94d0dcd5190afe126329866ae39cc9f00c2f324d
Ridhosyawali <ridhosyawali31@gmail.com>
2025-01-31 11:46:23 +0700

    useSWR

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,17 @@
+import Button from "@/components/atoms/Button";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
 
 const NotFound = () => {
   const router = useRouter();
-  const handleBackToHome = () => {
-    router.push("/");
-  };
+
   return (
     <>
-      <section class="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800">
-        <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+      <section class="flex items-center h-full p-16 dark:bg-gray-50 dark:text-gray-800 ">
+        <div class="container flex flex-col items-center justify-center px-5 mx-auto my-8 hover:animate-spin">
           <div class="max-w-md text-center">
-            <h2 class="mb-8 font-extrabold text-9xl dark:text-gray-400">
+            <h2 class="mb-8 font-extrabold text-9xl dark:text-gray-400 ">
               <span class="sr-only">Error</span>404
             </h2>
             <p class="text-2xl font-semibold md:text-3xl">
@@ -22,13 +21,13 @@ const NotFound = () => {
               But dont worry, you can find plenty of other things on our
               homepage.
             </p>
-            <a
-              rel="noopener noreferrer"
-              href={handleBackToHome}
-              className="px-8 py-3 font-semibold rounded dark:bg-violet-600 dark:text-gray-50"
+            <Button
+              onClick={() => router.back()}
+              href={"/products"}
+              className="px-8 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
             >
               Back to homepage
-            </a>
+            </Button>
           </div>
         </div>
       </section>
--- src/pages/products/[id]/index.js
+++ src/pages/products/[id]/index.js
@@ -1,26 +1,46 @@
 import { getProductById } from "@/services/product";
+import axios from "axios";
 import Image from "next/image";
-import { notFound } from "next/navigation";
 import React from "react";
+import useSWR from "swr";
 
+/**
+ * useSWR (Stale while revalidate) : hooks third party dari tim vercel
+ * digunakan untuk fetching data, caching dan validate di sisi client
+ * rumus : const data = useSWR("key atau endpoint", datafetcher);
+ * swr punya beberapa properti
+ * data: Data yang diambil dari API
+ * error: error handling saat mengambil data
+ * isLoading: status loading
+ * isValidating : status validasi ulang data(perbaharui data)
+ */
 const ProductDetailPage = ({ detailProduct }) => {
+  const api = process.env.NEXT_PUBLIC_API;
+
+  const { data } = useSWR(
+    `${api}/products/${detailProduct.id}`,
+    async () => {
+      const res = await axios.get(`${api}/products/${detailProduct.id}`);
+      return res.data;
+    },
+    {
+      initialData: detailProduct,
+    }
+  );
+
   return (
     <>
       <div className="flex flex-col px-5 py-5 bg-gradient-to-b from-black to-blue-900 min-h-screen">
         <h1 className="text-4xl font-bold text-white">Detail Product</h1>
         <div className="mt-5 rounded-2xl bg-white bg-opacity-20 h-fit p-4">
-          <h2 className="text-2xl font-bold text-white">
-            {detailProduct?.title}
-          </h2>
+          <h2 className="text-2xl font-bold text-white">{data?.title}</h2>
           <p className="text-white font-bold mt-5 text-xl">
-            {detailProduct?.description}
+            {data?.description}
           </p>
-          <p className="text-white text-xl font-bold mt-5">
-            {detailProduct?.price}
-          </p>
+          <p className="text-white text-xl font-bold mt-5">{data?.price}</p>
 
           <Image
-            src={detailProduct.image}
+            src={data?.image}
             width={400}
             height={400}
             alt="gambar"
@@ -33,7 +53,7 @@ const ProductDetailPage = ({ detailProduct }) => {
 };
 
 export async function getServerSideProps(context) {
-  console.log("ini isi context :", context.query.id);
+  //   console.log("ini isi context :", context.query.id);
 
   const id = context.query.id;
 
